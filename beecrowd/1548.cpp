9f88a7d22b1affcac4a5ae7347381a0a911242dd
João Augusto Marciano Silva <68918598+JoaoAugusto2020@users.noreply.github.com>
2024-08-12 11:33:49 -0300

    Add files via upload

--- beecrowd/1548.cpp
+++ beecrowd/1548.cpp
@@ -65,16 +65,73 @@ struct Fila {
         if (inicio == NULL) return 0;
         return inicio->valor;
     }
+    
+    void imprimir() {
+        No* aux = inicio;
+        while (aux != NULL) {
+            printf("%d ", aux->valor);
+            aux = aux->prox;
+        }
+        cout<<endl;
+    }
+    
+    int troca() {
+    	if (n == 0 || n == 1) return 0;
+        int trocas=0;
+        
+        No* aux = inicio;
+        No* anterior = inicio;
+        No* anterior2 = inicio;
+        while (aux != NULL) {
+            if(aux->valor >= aux->prox->valor){
+				aux = aux->prox;
+			}else if(aux->prox != NULL){
+				cout<<"precisa trocar"<<endl;
+				//anterior aponta para o próximo do atual
+				anterior->prox = aux->prox;
+				//o meu atual passa a apontar para o próximo do próximo
+				aux->prox = aux->prox->prox;
+				//o meu próximo passa a apontar para o autal
+				aux->prox->prox = anterior2->prox;
+				cout<<"trocou"<<endl;
+				
+				trocas++;
+			}
+			anterior = anterior->prox;
+			anterior2 = anterior2->prox;
+        }
+        
+        return trocas;
+    }
 
 };
 
 int main() {
 
-    Fila f;
-
-    f.enfileirar(10);
-    f.desenfileirar();
-    f.frente();
+	int filas;
+	
+	//rode n filas
+	cin>>filas;
+	while(filas--){
+		int qtd=0, totalTrocas=0;
+		Fila f;
+		
+		//criando a fila
+		cin>>qtd;
+		while(qtd--){
+			int nota=0;
+			cin>>nota;
+			f.enfileirar(nota);
+		}
+		
+		f.imprimir();
+		f.troca();
+		f.imprimir();
+		
+		//totalTrocas += f.troca();
+		
+		//cout<<totalTrocas<<endl;
+	}
 
     return 0;
 }
